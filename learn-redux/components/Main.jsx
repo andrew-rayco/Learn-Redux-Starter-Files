4e6842f1d5b93ea4e111b3c20d986fc2720d8563
Andrew <andy@andycondon.com>
2017-06-04 18:02:20 +1200

    ready for part 5

---
+++ learn-redux/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { link } from 'react-router'
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxstagram</Link>
+        </h1>
+      </div>
+    )
+  }
+})
+
+export default Main
